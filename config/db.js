7e76a1fb64b339b87dd3293028380d990a173268
Estela Valera <estela.valera96@gmail.com>
2024-10-15 12:42:41 +0200

    CORRECIÓN

--- config/db.js
+++ config/db.js
@@ -6,11 +6,8 @@ dotenv.config()
 
 const dbConnection = async() => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log('Base de datos conectada con éxito');
+        await mongoose.connect(process.env.MONGO_URI),
+            console.log('Base de datos conectada con éxito');
     } catch (error) {
         console.error(error);
         throw new Error('Error a la hora de iniciar la base de datos');
